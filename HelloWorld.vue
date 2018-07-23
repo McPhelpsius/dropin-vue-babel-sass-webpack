bf6ca560e0abd3afd3f76c2519832090e7c32a71
Nathan Phelps <nathanaphelps@gmail.com>
2018-07-22 22:09:31 -0500

    Everything

---
+++ HelloWorld.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="some-globally-inherited-style">
+    <h1>{{title}}</h1>
+    <p :style="{color: welcomeMessageColor}">
+      {{ welcomeMessage }}
+    </p>
+    <hr>
+    Welcome Message:
+    <input type="text" v-model="welcomeMessage">
+    <br>
+    Color:
+    <input type="text" v-model="welcomeMessageColor">
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: 'Hello World.',
+      welcomeMessage: 'Welcome to Vue',
+      welcomeMessageColor: this.initialWelcomeMessageColor,
+    }
+  },
+  props: {
+    initialWelcomeMessageColor: {
+      type: String,
+      default: '',
+    }
+  },
+  created() {},
+  methods: {
+    customConsoleLog(message) {
+      console.log(`${Date.now()} ${message}`);
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+  h3 { color: green; }
+</style>
+
+
